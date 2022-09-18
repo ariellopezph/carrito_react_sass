813598699cec99179415020a068a68385e358de4
ariellopezph <ariellopezph@gmail.com>
2022-09-18 16:04:39 -0300

    inicio del proyecto

---
+++ src/Component/Home/Index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Cart from '../Cart/Index';
+import Products from '../Products/Index';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Products />
+            <Cart />
+
+        </div>
+    )
+}
+
+export default Home
