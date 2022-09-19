3150809b918ff30bd0279f8410895e2823e86214
ariellopezph <ariellopezph@gmail.com>
2022-09-19 15:12:18 -0300

    Componentes y lista de productos

--- src/Component/Home/Index.jsx
+++ src/Component/Home/Index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Cart from '../Cart/Index';
 import Products from '../Products/Index';
-
+import styles from './styles.module.scss';
 
 const Home = () => {
     return (
-        <div>
+        <div className={styles.home}>
             <Products />
             <Cart />
 
--- src/Component/Products/Index.jsx
+++ src/Component/Products/Index.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import { ProductsData } from "../../Data/ProductData";
 
 
 const Products = () => {
     return (
-        <div>Products</div>
+        <div>
+            {ProductsData.map((product, i) => (
+                <div key={i}>
+                    <img src={product.img} alt={product.name} />
+                    <div>
+                        <p>
+                            {product.name} - ${product.price}
+                        </p>
+                    </div>
+                    <button onClick={() => console.log(product)}>Add to cart</button>
+                </div>
+
+            ))}
+        </div>
     )
 }
 
